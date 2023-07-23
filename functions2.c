50024e1a989ea3d16625b2e964fa4e2788eaf0e0
Hasssan888 <bakrimhassan944@gmail.com>
2023-07-23 17:05:29 +0200

    printf

--- functions2.c
+++ functions2.c
@@ -12,7 +12,7 @@
  * Return: Number of chars printed.
  */
 int print_pointer(va_list types, char buffer[],
-			int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
 	char extra_c = 0, padd = ' ';
 	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
@@ -27,13 +27,16 @@ int print_pointer(va_list types, char buffer[],
 		return (write(1, "(nil)", 5));
 	buffer[BUFF_SIZE - 1] = '\0';
 	UNUSED(precision);
+
 	num_addrs = (unsigned long)addrs;
+
 	while (num_addrs > 0)
 	{
 		buffer[ind--] = map_to[num_addrs % 16];
 		num_addrs /= 16;
 		length++;
 	}
+
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
 		padd = '0';
 	if (flags & F_PLUS)
@@ -41,7 +44,6 @@ int print_pointer(va_list types, char buffer[],
 	else if (flags & F_SPACE)
 		extra_c = ' ', length++;
 	ind++;
-	/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
 	return (write_pointer(buffer, ind, length,
 				width, flags, padd, extra_c, padd_start));
 }
@@ -58,7 +60,7 @@ int print_pointer(va_list types, char buffer[],
  * Return: Number of chars printed
  */
 int print_non_printable(va_list types, char buffer[],
-			int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
 	int i = 0, offset = 0;
 	char *str = va_arg(types, char *);
@@ -70,7 +72,6 @@ int print_non_printable(va_list types, char buffer[],
 
 	if (str == NULL)
 		return (write(1, "(null)", 6));
-
 	while (str[i] != '\0')
 	{
 		if (is_printable(str[i]))
@@ -96,7 +97,7 @@ int print_non_printable(va_list types, char buffer[],
  */
 
 int print_reverse(va_list types, char buffer[],
-			int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
 	char *str;
 	int i, count = 0;
@@ -107,23 +108,23 @@ int print_reverse(va_list types, char buffer[],
 	UNUSED(size);
 
 	str = va_arg(types, char *);
-
 	if (str == NULL)
 	{
 		UNUSED(precision);
-		str = ")Null(";
+		str = "(Null)";
+	}
+	for (i = 0; str[i]; i++)
+		;
+	for (i = i - 1; i >= 0; i--)
+	{
+		char z = str[i];
 
-		for (i = 0; str[i]; i++)
-			;
-		for (i = i - 1; i >= 0; i--)
-		{
-			char z = str[i];
-
-			write(1, &z, 1);
-			count++;
-		}
-		return (count);
+		write(1, &z, 1);
+		count++;
+	}
+	return (count);
 }
+
 /************************* PRINT A STRING IN ROT13 *************************/
 /**
  * print_rot13string - Print a string in rot13.
@@ -136,9 +137,10 @@ int print_reverse(va_list types, char buffer[],
  * Return: Numbers of chars printed
  */
 int print_rot13string(va_list types, char buffer[],
-			int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
-	char x, *str;
+	char x;
+	char *str;
 	unsigned int i, j;
 	int count = 0;
 	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
